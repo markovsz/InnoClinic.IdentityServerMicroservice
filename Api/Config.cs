7745f3af4040bc1993ac120fd9aae89549b83789
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-08 01:32:43 +0300

    add program structure

---
+++ Api/Config.cs
@@ -0,0 +1,52 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "Movies.Client",
+                    ClientName = "Movies Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("vfdsf&566efcn@!c_=".Sha256()) },
+                    AllowedScopes = { "Movie.API" }
+                }
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource
+                {
+
+                }
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("Movie.API", "Movie API")
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<TestUser> TestUsers =>
+            new TestUser[]
+            {
+                new TestUser
+                {
+
+                }
+            };
+    }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,26 @@
+using IdentityServer;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
