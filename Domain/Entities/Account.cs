5bd769b8b8ede638986f1ed5d705a872a6e4265e
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-10 15:56:32 +0300

    add entity

---
+++ Domain/Entities/Account.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities
+{
+    public class Account : IdentityUser
+    {
+        public Guid PhotoId { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
