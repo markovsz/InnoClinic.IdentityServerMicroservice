f96526987fed65fde4e07c13191ca15ccca1163f
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-10 15:58:21 +0300

    add db context + migrations

---
+++ Infrastructure/AuthDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AuthDbContext : IdentityDbContext<Account>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) 
+            : base(options)
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
