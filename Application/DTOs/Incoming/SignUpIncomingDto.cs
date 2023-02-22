40b69a75dc495591fa64763eb95a144dc9c7a9a3
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-22 17:30:20 +0300

    add DTOs

---
+++ Application/DTOs/Incoming/SignUpIncomingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.Incoming
+{
+    public class SignUpIncomingDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ReEnteredPassword { get; set; }
+    }
+}
---
+++ Application/DTOs/Outgoing/RefreshedTokenOutgoingDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.Outgoing
+{
+    public class RefreshedTokensOutgoingDto
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
