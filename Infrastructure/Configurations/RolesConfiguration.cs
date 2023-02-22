80a4f0e280ea249e3153798ec1c5eec45f15bd26
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-22 17:32:16 +0300

    move roles into enum

--- Infrastructure/Configurations/RolesConfiguration.cs
+++ Infrastructure/Configurations/RolesConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -12,22 +13,22 @@ namespace Infrastructure.Configurations
                 new IdentityRole
                 {
                     Id = "4ebec899-3482-4022-8f8e-33682f877eac",
-                    Name = "Doctor",
-                    NormalizedName = "DOCTOR",
+                    Name = nameof(UserRoles.Doctor),
+                    NormalizedName = nameof(UserRoles.Doctor).ToUpper(),
                     ConcurrencyStamp = Guid.NewGuid().ToString()
                 },
                 new IdentityRole
                 {
                     Id = "9c8ff47f-9018-4b0b-bbf4-ad7de2e67fbb",
-                    Name = "Patient",
-                    NormalizedName = "PATIENT",
+                    Name = nameof(UserRoles.Patient),
+                    NormalizedName = nameof(UserRoles.Patient).ToUpper(),
                     ConcurrencyStamp = Guid.NewGuid().ToString()
                 },
                 new IdentityRole
                 {
                     Id = "c317b51c-07c6-4bb7-a90e-f037c4e91141",
-                    Name = "Receptionist",
-                    NormalizedName = "RECEPTIONIST",
+                    Name = nameof(UserRoles.Receptionist),
+                    NormalizedName = nameof(UserRoles.Receptionist).ToUpper(),
                     ConcurrencyStamp = Guid.NewGuid().ToString()
                 }
             );
