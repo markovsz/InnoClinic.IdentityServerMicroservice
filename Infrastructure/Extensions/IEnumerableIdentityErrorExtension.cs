04bfcec078b7c92f1c17f5960e983eede8fe4652
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-22 16:56:48 +0300

    add IdentityResult's json converter

---
+++ Infrastructure/Extensions/IEnumerableIdentityErrorExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Extensions
+{
+    public static class IEnumerableIdentityErrorExtension
+    {
+        public static string AsJson(this IEnumerable<IdentityError> identityErrors)
+        {
+            var result =  JsonConvert.SerializeObject(identityErrors);
+            return result;
+        } 
+    }
+}
