367c3558950c69f8b40bb43ab689748198e84f65
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-13 13:33:25 +0300

    add email service

---
+++ Application/Interfaces/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces
+{
+    public interface IEmailService
+    {
+        void SendEmailConfirmation(string email, string confirmationPath);
+        void SendCredentials(string email, string password);
+    }
+}
